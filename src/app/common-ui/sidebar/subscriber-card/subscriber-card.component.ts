c4ea5f5522c71a3bba177648248af06b0428bd79
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-07-15 18:42:22 +0300

    feat: add ProfilePage and SubscriberCard links

--- src/app/common-ui/sidebar/subscriber-card/subscriber-card.component.ts
+++ src/app/common-ui/sidebar/subscriber-card/subscriber-card.component.ts
@@ -1,17 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../../data/interfaces/profile.interface';
 import { ImgUrlPipe } from "../../../helpers/pipes/img-url.pipe";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-subscriber-card',
-    imports: [ImgUrlPipe],
+    imports: [ImgUrlPipe, RouterLink],
     templateUrl: './subscriber-card.component.html',
     styleUrl: './subscriber-card.component.scss'
 })
 export class SubscriberCardComponent {
     @Input() profile!: Profile;
-
-    ngOnInit() {
-        console.log(this.profile)
-    }
 }
