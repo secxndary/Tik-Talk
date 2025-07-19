f499b32fcb3628a59413353718e600630be16ffd
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-07-19 17:38:08 +0300

    feat: add AvatarsPlaceholder w/ BoringAvatars API

---
+++ src/app/common-ui/avatar-placeholder/avatar-placeholder.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+
+@Component({
+    selector: 'app-avatar-placeholder',
+    templateUrl: 'avatar-placeholder.component.html',
+    styleUrl: 'avatar-placeholder.component.scss',
+    imports: [ImgUrlPipe]
+})
+export class AvatarPlaceholderComponent {
+    boringAvatarsApi = 'https://hostedboringavatars.vercel.app/api';
+    avatarType = 'marble';
+
+    @Input() class = '';
+    @Input() profile: Profile | undefined = undefined;
+
+    get avatarUrl(): string {
+        if (this.profile && this.profile.avatarUrl){
+            return this.profile.avatarUrl;
+        }
+
+        const profileUniqueProperty = this.profile?.id;
+
+        return `${this.boringAvatarsApi}/${this.avatarType}?name=${profileUniqueProperty}`;
+    }
+}
