f97c34041c5b322642225593d1bf7bc1c90a92a5
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-07-26 17:55:45 +0300

    feat: add post-input component

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
+import { AvatarPlaceholderComponent } from "../../../common-ui/avatar-placeholder/avatar-placeholder.component";
+import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
+import { ProfileService } from '../../../data/services/profile.service';
 
 @Component({
-  selector: 'app-post-input',
-  imports: [],
-  templateUrl: './post-input.component.html',
-  styleUrl: './post-input.component.scss'
+    selector: 'app-post-input',
+    imports: [AvatarPlaceholderComponent, SvgIconComponent],
+    templateUrl: './post-input.component.html',
+    styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
+    r2 = inject(Renderer2);
+    profile = inject(ProfileService).me();
 
+    onTextAreaInput(event: Event) {
+        const textarea = event.target as HTMLTextAreaElement;
+
+        this.r2.setStyle(textarea, 'height', 'auto');
+        this.r2.setStyle(textarea, 'height', `${textarea.scrollHeight}px`);
+    }
 }
