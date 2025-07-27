ec3771a36411bf261762acfb29969a8ff840e27e
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-07-27 18:19:29 +0300

    feat: add getPosts(), getMyPosts(), fix createPost

--- src/app/data/services/post.service.ts
+++ src/app/data/services/post.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Post, PostCreateDto } from "../interfaces/post.interface";
+import { ProfileService } from "./profile.service";
+import { of, switchMap, tap } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -11,7 +13,37 @@ export class PostService {
 
     http = inject(HttpClient);
 
+    me = inject(ProfileService).me();
+    posts = signal<Post[]>([]);
+
+    getMyPosts() {
+        return this.http.get<Post[]>(`${this.postApiUrl}/`)
+            .pipe(
+                tap(val => this.posts.set(val))
+            );
+    }
+
+    getPosts(accountId: number | undefined) {
+        if (!accountId) {
+            return of(undefined);
+        }
+
+        return this.http.get<Post[]>(
+            `${this.postApiUrl}/`,
+            {
+                params: {
+                    user_id: accountId
+                }
+            })
+            .pipe(
+                tap(val => this.posts.set(val))
+            );
+    }
+
     createPost(post: PostCreateDto) {
-        return this.http.post<Post>(`${this.postApiUrl}/`, post);
+        return this.http.post<Post>(`${this.postApiUrl}/`, post)
+            .pipe(
+                switchMap(() => this.getMyPosts())  //  getMyPosts или getPosts(id)?
+            );
     }
 }
