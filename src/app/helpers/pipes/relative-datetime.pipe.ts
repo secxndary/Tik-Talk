c6be6fa7e5269858dc1a7e5d71f91c250322e84d
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-08-04 08:38:25 +0300

    feat: RelativeDateTime pipe

--- src/app/helpers/pipes/relative-datetime.pipe.ts
+++ src/app/helpers/pipes/relative-datetime.pipe.ts
@@ -1,16 +1,26 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { DateTime, Interval } from "luxon";
+import { DateTime } from "luxon";
 
 @Pipe({
     name: 'relativeDateTime'
 })
 export class RelativeDateTimePipe implements PipeTransform {
     transform(value: string | null) {
-        if (!value)
+        if (!value) {
             return null;
+        }
 
-        const date = DateTime.fromISO(value)
+        const date = DateTime.fromISO(value).setLocale('ru-RU');;
+        const dateInGmtPlus3 = date.plus({ hours: 3 });
 
-        return date.plus({ hours: 3 }).toRelative();
+        if (dateInGmtPlus3.diffNow('days').days > -4) {
+            return dateInGmtPlus3.toRelative();
+        }
+
+        if (dateInGmtPlus3.diffNow('years').years <= -1) {
+            return dateInGmtPlus3.toFormat('d MMMM yyyy в HH:mm');
+        }
+
+        return dateInGmtPlus3.toFormat('d MMMM в HH:mm');
     }
 }
