749fed5e2a6e9c81d9c2aa1433937098deb84ecc
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-08-04 10:31:35 +0300

    feat: Post component w/ image, dateTime & content

--- src/app/pages/profile-page/post/post.component.ts
+++ src/app/pages/profile-page/post/post.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { Post } from '../../../data/interfaces/post.interface';
+import { AvatarPlaceholderComponent } from '../../../common-ui/avatar-placeholder/avatar-placeholder.component';
+import { ProfileNamePipe } from '../../../helpers/pipes/profile-name.pipe';
+import { RouterLink } from '@angular/router';
+import { RelativeDateTimePipe } from '../../../helpers/pipes/relative-datetime.pipe';
 
 @Component({
-  selector: 'app-post',
-  imports: [],
-  templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+    selector: 'app-post',
+    imports: [AvatarPlaceholderComponent, ProfileNamePipe, RouterLink, RelativeDateTimePipe],
+    templateUrl: './post.component.html',
+    styleUrl: './post.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostComponent {
-
+    post = input<Post>();
+    author = computed(this.post)()?.author;
 }
